0513a28ac63de1bf6e1fca2987fce0776e8b89a5
Pavel Filonov <qqq.qqq48@yandex.ru>
2022-05-30 10:31:57 +0300

    Profile done

---
+++ app/src/main/java/com/example/moneywayapp/ProfileFragment.java
@@ -0,0 +1,105 @@
+package com.example.moneywayapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.moneywayapp.api.HelperAPI;
+import com.example.moneywayapp.api.UserAPI;
+import com.example.moneywayapp.model.Empty;
+import com.example.moneywayapp.model.User;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ProfileFragment extends Fragment {
+
+    private static final String TAG = ProfileFragment.class.getSimpleName();
+
+    private EditText emailText, loginText, passwordText;
+
+    private User user;
+
+    private UserAPI userAPI;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        TextView loginTextView = requireView().findViewById(R.id.usernameProfileTextView);
+        emailText = requireView().findViewById(R.id.editTextProfileEmail);
+        loginText = requireView().findViewById(R.id.editTextProfileUsername);
+        passwordText = requireView().findViewById(R.id.editTextProfilePassword);
+        Button updateEmailButton = requireView().findViewById(R.id.updateEmailButton);
+        Button updateLoginButton = requireView().findViewById(R.id.updateLoginButton);
+        Button updatePasswordButton = requireView().findViewById(R.id.updatePasswordButton);
+
+        initUser();
+
+        loginTextView.setText(user.getLogin());
+
+        updateEmailButton.setOnClickListener(this::onClickedUpdateEmailButton);
+        updateLoginButton.setOnClickListener(this::onClickedUpdateLoginButton);
+        updatePasswordButton.setOnClickListener(this::onClickedUpdatePasswordButton);
+
+        return inflater.inflate(R.layout.profile, container, false);
+    }
+
+    private void onClickedUpdateEmailButton(View view) {
+        Call<Empty> call = userAPI.updateEmail(emailText.toString());
+        callEnqueue(call, "Email изменён");
+    }
+
+    private void onClickedUpdateLoginButton(View view) {
+        Call<Empty> call = userAPI.updateLogin(loginText.toString());
+        callEnqueue(call, "Логин изменён");
+    }
+
+    private void onClickedUpdatePasswordButton(View view) {
+        Call<Empty> call = userAPI.updatePassword(passwordText.toString());
+        callEnqueue(call, "Пароль изменён");
+    }
+
+    private void callEnqueue(Call<Empty> call, String message) {
+        call.enqueue(new Callback<Empty>() {
+            @Override
+            public void onResponse(Call<Empty> call, Response<Empty> response) {
+                Log.i(TAG, message);
+                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Empty> call, Throwable t) {
+                Log.i(TAG, t.getMessage());
+                Toast.makeText(getContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void initUser() {
+        userAPI = HelperAPI.getRetrofit().create(UserAPI.class);
+        Call<User> call = userAPI.profile();
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                user = response.body();
+                Log.i(TAG, "Удачное подключение к профилю");
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.w(TAG, t.getMessage());
+                Toast.makeText(getContext(), "Не удалось подключиться к профилю", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/moneywayapp/api/UserAPI.java
+++ app/src/main/java/com/example/moneywayapp/api/UserAPI.java
@@ -5,7 +5,10 @@ import com.example.moneywayapp.model.User;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Query;
 
 public interface UserAPI {
 
@@ -14,4 +17,16 @@ public interface UserAPI {
 
     @POST("register")
     Call<Empty> register(@Body User user);
+
+    @GET("users/profile")
+    Call<User> profile();
+
+    @PUT("users/profile/email")
+    Call<Empty> updateEmail(@Query(value = "email") String email);
+
+    @PUT("users/profile/login")
+    Call<Empty> updateLogin(@Query(value = "login") String login);
+
+    @PUT("users/profile/password")
+    Call<Empty> updatePassword(@Query(value = "password") String password);
 }
